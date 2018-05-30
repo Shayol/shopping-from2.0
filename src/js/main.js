d42967f5e7eb160abcab0fd6fe7f2a8d8027a275
sudoQ <vorga88@gmail.com>
2018-05-31 00:35:19 +0300

    add js for continue button, positioning for payment fields

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
 import "../scss/main.scss";
+
+window.addEventListener('load', function () { 
+
+    NodeList.prototype.forEach = Array.prototype.forEach;
+    var formSections = document.querySelectorAll('.form__section:not(:last-child)');
+
+
+    formSections.forEach(section => {
+        section.addEventListener('click', (event) => {
+            var el = event.target;
+            if(el.classList.contains('button')) {
+                section.style.display = 'none';
+                section.nextElementSibling.style.display = "block";
+            }
+        });
+    });
+});
